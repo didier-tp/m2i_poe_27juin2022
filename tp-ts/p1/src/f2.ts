173c000682db65c405306b25c8acd2d828ce88bf
DidierDefrance <didier@d-defrance.fr>
2022-09-02 12:16:18 +0200

    s

--- tp-ts/p1/src/f2.ts
+++ tp-ts/p1/src/f2.ts
@@ -10,3 +10,15 @@ let b = false; //en affectant une valeur au même moment que la déclaration ,
 //b = "abc"; //refusé par typescript qui considère que b est un boolean
 b=true;
 console.log('b='+b);
+
+let s1 = "abc" ; // considéré par typescript comme étant de type :string
+s1 ="def";
+//s1=6; //refusé
+//s1=null; //refusé
+//s1=undefined;//refusé
+console.log("s1="+s1);
+
+let s2 : string | null ;
+s2 = "abc";
+s2 = null;
+console.log("s2="+s2);
---
+++ tp-ts/p1/src/f3.ts
@@ -0,0 +1,32 @@
+let tab1 : number[];
+tab1 = [ 6 , 2 , 8 ];
+//tab1 = [ 6 , 2 , 8 , "abc"] ; //refusé
+console.log("longueur de tab1="+tab1.length);
+tab1.push(12) ;//ajoute la valeur 12 en fin de tableau
+//console.log("tab1="+tab1);
+console.log("tab1 au format JSON="+JSON.stringify(tab1));
+
+//déclarer et intialiser un tableau de string
+//tabJours avec des valeurs "lundi" , "mardi" , ....
+let tabJours : string[];
+tabJours = [ "lundi" , "mardi" , "mercredi" , "jeudi" , "vendredi"];
+tabJours.push("samedi"); tabJours.push("dimanche");
+
+//construire un autre tableau tabJoursMaj à partir du tableau tabJours
+//avec des valeurs de "LUNDI" , ...
+let tabJoursMaj : string[] = []; //tableau vide au tout début
+/*
+for(let i=0;i<tabJours.length;i++){
+    tabJoursMaj.push(tabJours[i].toUpperCase());
+}
+*/
+for(let i in tabJours){
+    tabJoursMaj.push(tabJours[i].toUpperCase());
+}
+/*
+for(let jour of tabJours){
+    tabJoursMaj.push(jour.toUpperCase());
+}
+*/
+//afficher tabJoursMaj
+console.log("tabJoursMaj au format JSON="+JSON.stringify(tabJoursMaj));
