bf9d4b37c140455f13f4abbd04835fc2740cdb74
DidierDefrance <didier@d-defrance.fr>
2022-09-02 14:42:47 +0200

    avec get/set

--- tp-ts/p1/dist/js/personne.js
+++ tp-ts/p1/dist/js/personne.js
@@ -3,7 +3,16 @@ class Personne {
     constructor(prenom = "?", nom = "?", age = 0) {
         this.prenom = prenom;
         this.nom = nom;
-        this.age = age;
+        this._age = age;
+    }
+    get age() {
+        return this._age;
+    }
+    set age(age) {
+        if (age >= 0)
+            this._age = age;
+        else
+            throw "age negatif invalide";
     }
     incrementerAge() {
         this.age++;
@@ -12,6 +21,14 @@ class Personne {
 let p1;
 p1 = new Personne("jean", "Bon", 45);
 p1.incrementerAge(); //45ans vers 46ans
+console.log("p1=" + JSON.stringify(p1));
+try {
+    p1.age = -5; // ça appelle automatiquement set age(-5)
+}
+catch (ex) {
+    console.log("exception attendue=" + ex);
+}
+p1.age = 35; // ça appelle automatiquement set age(35)
 console.log("p1=" + JSON.stringify(p1));
 let p2 = new Personne(); //avec valeurs par défaut "?" , "?" , 0
 p2.incrementerAge(); //0an vers 1an
--- tp-ts/p1/src/personne.ts
+++ tp-ts/p1/src/personne.ts
@@ -1,12 +1,22 @@
 class Personne{
-   prenom : string ; 
+   prenom : string ;
    nom : string;
-   age : number;
+   private _age : number;
+
+   public get age():number {
+    return this._age;
+   }
+
+   public set age(age:number){
+      if(age>=0) 
+          this._age = age;
+      else throw "age negatif invalide";
+   }
 
    constructor(prenom : string ="?" , nom :string = "?"  , age : number = 0){
     this.prenom = prenom;
     this.nom = nom;
-    this.age = age;
+    this._age = age;
    }
 
    incrementerAge():void{
@@ -14,10 +24,16 @@ class Personne{
    }
 }
 
-
 let p1 : Personne;
 p1=new Personne("jean", "Bon" , 45);
 p1.incrementerAge(); //45ans vers 46ans
+console.log("p1="+ JSON.stringify(p1));
+try{
+  p1.age = -5; // ça appelle automatiquement set age(-5)
+}catch(ex){
+    console.log("exception attendue=" + ex);
+}
+p1.age = 35;// ça appelle automatiquement set age(35)
 console.log("p1="+ JSON.stringify(p1));
 
 let p2 = new Personne(); //avec valeurs par défaut "?" , "?" , 0
