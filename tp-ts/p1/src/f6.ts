f6cd00169918666b61b8c9af676e1c54dfafd244
DidierDefrance <didier@d-defrance.fr>
2022-09-05 11:53:18 +0200

    f6 et generic

---
+++ tp-ts/p1/src/f6.ts
@@ -0,0 +1,13 @@
+class ProduitV2<T>{
+    constructor(public ref : T ,
+                public label : string = "?",
+                public prix : number = 0.0){
+    }
+}
+
+  
+ let pa =  new ProduitV2<number>(1,"cahier",3.6);
+ console.log("pa="+JSON.stringify(pa));
+
+ let pb =  new ProduitV2<string>("r234","cahier",3.6);
+ console.log("pb="+JSON.stringify(pb));
