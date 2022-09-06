23461e6c2acac6f700019ceefea20587427eb12b
DidierDefrance <didier@d-defrance.fr>
2022-09-06 16:41:21 +0200

    tva version 1 (avec bouton poussoir)

--- tp-ts/my-app/src/app/basic/tva/tva.component.ts
+++ tp-ts/my-app/src/app/basic/tva/tva.component.ts
@@ -7,6 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TvaComponent implements OnInit {
 
+  ht :number = 0;
+  tauxTva : number =0; //en % (0 , 10, 20)
+  tva : number = 0;
+  ttc : number = 0;
+
+  onCalculerTvaEtTtc(){
+    this.tva = this.ht * this.tauxTva / 100;
+    this.ttc = Number(this.ht) + Number(this.tva);
+  }
+
   constructor() { }
 
   ngOnInit(): void {
