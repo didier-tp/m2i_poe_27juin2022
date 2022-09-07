c32cad6a2706fb8530ab6f4dd6fd788b6163697b
DidierDefrance <didier@d-defrance.fr>
2022-09-07 11:18:21 +0200

    s

--- tp-ts/my-app/src/app/app-routing.module.ts
+++ tp-ts/my-app/src/app/app-routing.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { BasicComponent } from './basic/basic.component';
+import { ProduitsComponent } from './produits/produits.component';
 import { WelcomeComponent } from './welcome/welcome.component';
 
 const routes: Routes = [
   { path: 'welcome', component: WelcomeComponent },
   { path: '', redirectTo: '/welcome', pathMatch: 'full'},
-  { path: 'basic', component: BasicComponent }
+  { path: 'basic', component: BasicComponent } ,
+  { path: 'produits', component: ProduitsComponent }
 ];
 
 @NgModule({
