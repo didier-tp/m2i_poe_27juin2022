4bb051e96a683208a6c8eaa6058bd92221579505
DidierDefrance <didier@d-defrance.fr>
2022-09-07 11:32:04 +0200

    tableau

--- tp-ts/my-app/src/app/produits/produits.component.ts
+++ tp-ts/my-app/src/app/produits/produits.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Produit } from '../common/produit';
 
 @Component({
   selector: 'app-produits',
@@ -6,6 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./produits.component.scss']
 })
 export class ProduitsComponent implements OnInit {
+
+  tabProduits :Produit[] = [
+    new Produit(1 , "cahier" , 3.5),
+    new Produit(2 , "stylo" , 1.5) ,
+    new Produit(3 , "classeur" , 5.5)
+  ];
 
   constructor() { }
 
