543ea78bf6c0deda298e9311d7894783593e609b
DidierDefrance <didier@d-defrance.fr>
2022-09-07 14:22:41 +0200

    preferenceService

--- tp-ts/my-app/src/app/header/header.component.ts
+++ tp-ts/my-app/src/app/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { PreferenceService } from '../common/preference.service';
 
 @Component({
   selector: 'app-header',
@@ -7,7 +8,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  constructor(public preferenceService : PreferenceService) { 
+    //injection de dépendance effectuée automatiquement 
+    //sur preferenceService
+  }
 
   ngOnInit(): void {
   }
