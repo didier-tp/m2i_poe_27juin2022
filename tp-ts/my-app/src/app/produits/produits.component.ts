57c087b07d48d0e1d93232546521989665949603
DidierDefrance <didier@d-defrance.fr>
2022-09-07 11:42:01 +0200

    s

--- tp-ts/my-app/src/app/produits/produits.component.ts
+++ tp-ts/my-app/src/app/produits/produits.component.ts
@@ -8,11 +8,18 @@ import { Produit } from '../common/produit';
 })
 export class ProduitsComponent implements OnInit {
 
+  produit : Produit = new Produit(); //produit selectionné ou bien nouveau produit
+
+  //tableau des produits existants et à efficher:
   tabProduits :Produit[] = [
     new Produit(1 , "cahier" , 3.5),
     new Produit(2 , "stylo" , 1.5) ,
     new Produit(3 , "classeur" , 5.5)
   ];
+
+  onAjoutProduit(){
+    this.tabProduits.push(this.produit);//v1 
+  }
 
   constructor() { }
 
